83ca34f2ad1dec2ba27d5ad26c966ae9c10e8e9f
Zeyad Waled <172551922+Zeyad-101@users.noreply.github.com>
2026-05-03 22:51:38 +0300

    Add files via upload

---
+++ BMO main/secrets.h
@@ -0,0 +1,8 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+const char* WIFI_SSID = "Enter_your_wifi_name";
+const char* WIFI_PASSWORD = "Enter_your_wifi_password";
+const char* OPENAI_API_KEY = "Enter your Open ai API";
+
+#endif
